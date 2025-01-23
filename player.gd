6b6014827e980916ae213f28ddc8fbe12424315e
Judah Godden <339633265@gotvdsb.ca>
2025-01-23 01:17:17 -0500

    working

--- player.gd
+++ player.gd
@@ -1,15 +1,37 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var acceleration = 5
-@export var max_speed = 50
-var screensize = Vector2(800, 800)
+@export var wheel_base = 30
+@export var steering_angle = 15
+@export var speed = 500
 
+var velocity = Vector2.ZERO
+var steering_direction
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta):
+	get_input()
+	calculate_steering(delta)
+	velocity = move_and_slide(velocity)
 
+func get_input():
+	var direction = 0
+	if Input.is_action_pressed("steer_right"):
+		direction += 1
+	if Input.is_action_pressed("steer_left"):
+		direction -= 1
+	steering_direction = direction * deg_to_rad(steering_angle)
+	
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("accelerate"):
+		velocity.x = transform.x * 500
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2
+	var front_wheel = position + transform.x * wheel_base / 2
+	
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steering_direction) * delta
+	
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	
+	velocity = new_heading * velocity.length()
+	rotation = new_heading.angle()
